92646e1697255b5b7dd0125698d3afb6b3a7d3f8
Niran Babalola <niran@niran.org>
2011-03-16 16:48:37 -0500

    Test pulling subclassed items from a queryset

--- armstrong/core/arm_content/tests/_utils.py
+++ armstrong/core/arm_content/tests/_utils.py
@@ -15,3 +15,14 @@ def create_random_article(**options):
     }
     data.update(options)
     return Article.objects.create(**data)
+
+def create_random_video(**options):
+    random_int = random.randint(1000, 9999)
+    data = {
+        'pub_date': datetime.now(),
+        'pub_status': PUB_STATUSES['Published'],
+        'title': 'Random Article %s' % random_int,
+        'youtube_id': str(random_int),
+    }
+    data.update(options)
+    return Video.objects.create(**data)

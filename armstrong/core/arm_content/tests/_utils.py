e9c0481682a3fb66f6e61143144d292606ff78c9
Travis Swicegood <development@domain51.com>
2011-04-11 16:33:17 -0500

    Extract methods for use in other areas of the tests

--- armstrong/core/arm_content/tests/_utils.py
+++ armstrong/core/arm_content/tests/_utils.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from django.contrib.auth.models import User
 from django.test import TestCase
 import random
 
 from .arm_content_support.models import Article, Video
+from ..models import Authors
 from ..publication.constants import PUB_STATUSES
 
 
@@ -27,3 +29,31 @@ def create_random_video(**options):
     }
     data.update(options)
     return Video.objects.create(**data)
+
+
+def generate_random_user():
+    r = random.randint(10000, 20000)
+    return User.objects.create(username="random-user-%d" % r,
+            first_name="Some", last_name="Random User %d" % r)
+
+
+def generate_random_users(n=2):
+    return [generate_random_user() for i in range(n)]
+
+
+def add_n_users_to_authors(authors, *users):
+    for user in users:
+        authors.users.add(user)
+
+
+def add_profile_to(profile_class, *users):
+    for user in users:
+        profile = profile_class.objects.create(user=user)
+        user._profile_cache = profile
+
+
+def generate_authors_with_two_users():
+    authors = Authors.objects.create()
+    bob, alice = generate_random_users()
+    add_n_users_to_authors(authors, bob, alice)
+    return authors, bob, alice
--- armstrong/core/arm_content/tests/models/authors.py
+++ armstrong/core/arm_content/tests/models/authors.py
@@ -1,40 +1,10 @@
-from django.contrib.auth.models import User
 import fudge
 import random
 
 from ..arm_content_support.models import SimpleProfile
 from .._utils import *
 
-from ...models import Authors
 from ... import models
-
-
-def generate_random_user():
-    r = random.randint(10000, 20000)
-    return User.objects.create(username="random-user-%d" % r,
-            first_name="Some", last_name="Random User %d" % r)
-
-
-def generate_random_users(n=2):
-    return [generate_random_user() for i in range(n)]
-
-
-def add_n_users_to_authors(authors, *users):
-    for user in users:
-        authors.users.add(user)
-
-
-def add_profile_to(profile_class, *users):
-    for user in users:
-        profile = profile_class.objects.create(user=user)
-        user._profile_cache = profile
-
-
-def generate_authors_with_two_users():
-    authors = Authors.objects.create()
-    bob, alice = generate_random_users()
-    add_n_users_to_authors(authors, bob, alice)
-    return authors, bob, alice
 
 
 class AuthorsModelTestCase(TestCase):

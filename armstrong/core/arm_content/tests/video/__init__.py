365e0febf73a5b56e08866219b9107ad48da20b9
Travis Swicegood <development@domain51.com>
2011-04-18 11:16:30 -0500

    Start down the path toward a fully-fleshed out MultipleBackends object

--- armstrong/core/arm_content/tests/video/__init__.py
+++ armstrong/core/arm_content/tests/video/__init__.py
@@ -12,8 +12,11 @@ class SomeOtherTestableBackend(object):
 
 
 class GetBackendTestCase(TestCase):
+    def backend_name(self, name):
+        return "%s.%s" % (self.__module__, name)
+
     def test_uses_backend_from_settings(self):
-        backend_name = "%s.TestableBackend" % self.__module__
+        backend_name = self.backend_name('TestableBackend')
         settings = fudge.Fake()
         settings.has_attr(ARMSTRONG_EXTERNAL_VIDEO_BACKEND=backend_name)
 
@@ -22,7 +25,7 @@ class GetBackendTestCase(TestCase):
             self.assertEqual(backend, TestableBackend)
 
     def test_uses_injected_settings_if_provided(self):
-        backend_name = "%s.SomeOtherTestableBackend" % self.__module__
+        backend_name = self.backend_name('SomeOtherTestableBackend')
         settings = fudge.Fake()
         settings.has_attr(ARMSTRONG_EXTERNAL_VIDEO_BACKEND=backend_name)
 
@@ -30,3 +33,28 @@ class GetBackendTestCase(TestCase):
 
         backend = backends.get_backend(settings=settings)
         self.assertEqual(backend, SomeOtherTestableBackend)
+
+    def test_returns_MultipleBackend_if_configured_with_a_list(self):
+        my_backends = [self.backend_name('TestableBackend'),
+                self.backend_name('SomeOtherTestableBackend')]
+
+        settings = fudge.Fake()
+        settings.has_attr(ARMSTRONG_EXTERNAL_VIDEO_BACKEND=my_backends)
+
+        backend = backends.get_backend(settings=settings)
+        self.assertEqual(backend, backends.MultipleBackends)
+
+
+class MultipleBackendsTestCase(TestCase):
+    def test_each_backend_is_called_until_one_answers_with_non_none(self):
+        expected = ("foo", "bar", random.randint(100, 200))
+        one = fudge.Fake()
+        one.expects('parse').with_args('foo/bar').returns(None)
+
+        two = fudge.Fake()
+        two.expects('parse').with_args('foo/bar').returns(expected)
+
+        fudge.clear_calls()
+
+        backend = backends.MultipleBackends(one, two)
+        self.assertEqual(expected, backend.parse('foo/bar'))
--- armstrong/core/arm_content/video/backends/__init__.py
+++ armstrong/core/arm_content/video/backends/__init__.py
@@ -7,12 +7,27 @@ from django.utils.importlib import import_module
 from .youtube import YouTubeBackend
 
 
+class MultipleBackends(object):
+    def __init__(self, *others):
+        self.others = others
+
+    def parse(self, url):
+        for other in self.others:
+            result = other.parse(url)
+            if result is not None:
+                return result
+
+
 def get_backend(settings=None):
     if not settings:
         settings = default_settings
 
-    # TODO: Should raise an ImproperlyConfigured error if this isn't
-    #       present in the settings variable.
-    module, backend_class = settings.ARMSTRONG_EXTERNAL_VIDEO_BACKEND.rsplit(".", 1)
-    backend_module = import_module(module)
-    return getattr(backend_module, backend_class)
+    backend_name = settings.ARMSTRONG_EXTERNAL_VIDEO_BACKEND
+    if type(backend_name) is str:
+        # TODO: Should raise an ImproperlyConfigured error if this isn't
+        #       present in the settings variable.
+        module, backend_class = settings.ARMSTRONG_EXTERNAL_VIDEO_BACKEND.rsplit(".", 1)
+        backend_module = import_module(module)
+        return getattr(backend_module, backend_class)
+    else:
+        return MultipleBackends

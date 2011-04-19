0b1519e7edbb7cdb4c0f8b10252e3fb7bb1bcca5
Travis Swicegood <development@domain51.com>
2011-04-19 10:45:01 -0500

    Make sure that an embed on the YouTubeBackend it returns the proper HTML

--- armstrong/core/arm_content/tests/video/backends/youtube.py
+++ armstrong/core/arm_content/tests/video/backends/youtube.py
@@ -21,3 +21,16 @@ class YouTubeBackendTestCase(TestCase):
 
         video = EmbeddedVideo(url, backend)
         self.assertEqual(random_id, video.id)
+
+    def test_returns_the_expected_html_when_embed_is_called(self):
+        random_id = str(random.randint(100, 200))
+        url = "http://youtube.com/watch?v=%s" % random_id
+        backend = YouTubeBackend()
+
+        video = EmbeddedVideo(url, backend)
+        expected = "".join([
+            '<iframe title="YouTube video player" ',
+            'width="640" height="390" ' ,
+            'src="http://www.youtube.com/embed/%s" ',
+            'frameborder="0" allowfullscreen></iframe>']) % random_id
+        self.assertEqual(expected, backend.embed(video))

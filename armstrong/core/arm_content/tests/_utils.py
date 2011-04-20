4f7f14b96e1edce735fa8923b9166e05886ddb74
Travis Swicegood <development@domain51.com>
2011-04-20 16:18:18 -0500

    add in code to generate staff users

--- armstrong/core/arm_content/tests/_utils.py
+++ armstrong/core/arm_content/tests/_utils.py
@@ -37,6 +37,25 @@ def generate_random_user():
             first_name="Some", last_name="Random User %d" % r)
 
 
+def generate_random_staff_users(n=2):
+    orig_users = generate_random_users(n)
+    users = User.objects.filter(pk__in=[a.id for a in orig_users])
+    users.update(is_staff=True)
+    return [a for a in users]
+
+class generate_random_staff_usersTestCase(TestCase):
+    def test_returns_2_users_by_default(self):
+        self.assertEqual(len(generate_random_staff_users()), 2)
+
+    def test_returns_n_users(self):
+        r = random.randint(1, 5)
+        self.assertEqual(len(generate_random_staff_users(r)), r)
+
+    def test_all_users_are_staff(self):
+        users = generate_random_staff_users()
+        for user in users:
+            self.assertTrue(user.is_staff)
+
 def generate_random_users(n=2):
     return [generate_random_user() for i in range(n)]
 

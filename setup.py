8ef869d7910280e0d5116ce12a504972976807e6
Travis Swicegood <development@domain51.com>
2011-04-25 12:35:48 -0500

    dynamically generate the packages inside setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
-from setuptools import setup
+from distutils.core import setup
+import os
+
+# Borrowed and modified from django-registration
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+def build_package(dirpath, dirnames, filenames):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    pkg = dirpath.replace(os.path.sep, '.')
+    if os.path.altsep:
+        pkg = pkg.replace(os.path.altsep, '.')
+    packages.append(pkg)
+
+[build_package(dirpath, dirnames, filenames) for dirpath, dirnames, filenames in os.walk('armstrong')]
 
 setup(
     name='armstrong.core.arm_content',
@@ -8,17 +28,7 @@ setup(
     author_email='tech@texastribune.org',
     url='http://github.com/texastribune/armstrong.core.arm_content/',
     # TODO: generate this dynamically
-    packages=[
-        'armstrong',
-        'armstrong.core',
-        'armstrong.core.arm_content',
-        'armstrong.core.arm_content.tests',
-        'armstrong.core.arm_content.tests.fields',
-        'armstrong.core.arm_content.tests.models',
-        'armstrong.core.arm_content.video',
-        'armstrong.core.arm_content.video.backends',
-    ],
-
+    packages=packages,
     install_requires=[
         'distribute',
     ],

dd2f211e488c599efdf0f8c29ee7376909d704ef
Travis Swicegood <development@domain51.com>
2011-04-26 10:42:14 -0500

    fix typo

--- armstrong/core/arm_content/models.py
+++ armstrong/core/arm_content/models.py
@@ -1,4 +1,4 @@
 # Here for Django's sake.
 #
 # This app doesn't ship with any models, just Mixins and Fields needed to
-# provide the basic functionality of Django.
+# provide the basic functionality of Armstrong.

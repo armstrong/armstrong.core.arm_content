d488e695abbc890e47737f9a86d73ae2e038a802
Travis Swicegood <development@domain51.com>
2011-05-06 12:43:11 -0500

    convert over to new armstrong.dev fabfile

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,4 +1,4 @@
-from ._utils import *
+from armstrong.dev.tasks import *
 
 
 settings = {
@@ -10,33 +10,12 @@ settings = {
         'django.contrib.sessions',
         'armstrong.core.arm_content',
         'armstrong.core.arm_content.tests.arm_content_support',
+        'south',
     ),
     'AUTH_PROFILE_MODULE': 'arm_content_support.SimpleProfile',
     'ROOT_URLCONF': 'armstrong.core.arm_content.tests.arm_content_support.urls',
     'ARMSTRONG_EXTERNAL_VIDEO_BACKEND': 'armstrong.core.arm_content.video.backends.YouTubeBackend'
 }
 
-
-@task
-def clean():
-    local('find . -name "*.py[co]" -exec rm {} \;')
-
-
-@task
-def pep8():
-    local('find ./armstrong -name "*.py" | xargs pep8', capture=False)
-
-
-@task
-def test():
-    with html_coverage_report():
-        run_tests(settings, 'arm_content_support', 'arm_content')
-
-
-@task
-def runserver():
-    from d51.django.virtualenv.base import VirtualEnvironment
-    runner = VirtualEnvironment()
-    runner.run(settings)
-    runner.call_command("syncdb")
-    runner.call_command("runserver")
+main_app = "arm_content"
+tested_apps = ("arm_content_support", "arm_content", )

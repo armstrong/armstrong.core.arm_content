8ce1550213dbbbf9cab83081843736073eba096a
Travis Swicegood <development@domain51.com>
2011-05-10 10:49:25 -0500

    Pull this code in so it can be used to test abstracts
    This needs to be moved into armstrong.dev or into an armstrong.test
    package so it's not duplicated across all of the apps that need it.

--- armstrong/core/arm_content/tests/_utils.py
+++ armstrong/core/arm_content/tests/_utils.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from django.contrib.auth.models import User
+from django.core.management.color import no_style
+from django.db import connection
 from django.test import TestCase as DjangoTestCase
 import fudge
 import random
@@ -7,6 +9,59 @@ import unittest
 
 from .arm_content_support.models import Article, Video
 from ..publication.constants import PUB_STATUSES
+
+
+def create_concrete_table(func=None, model=None):
+    style = no_style()
+    seen_models = connection.introspection.installed_models(
+            connection.introspection.table_names())
+
+    def actual_create(model):
+        sql, _references = connection.creation.sql_create_model(model, style,
+                seen_models)
+        cursor = connection.cursor()
+        for statement in sql:
+            cursor.execute(statement)
+
+    if func:
+        def inner(self, *args, **kwargs):
+            func(self, *args, **kwargs)
+            actual_create(self.model)
+        return inner
+    elif model:
+        actual_create(model)
+
+
+def destroy_concrete_table(func=None, model=None):
+    style = no_style()
+    # Assume that there are no references to destroy, these are supposed to be
+    # simple models
+    references = {}
+
+    def actual_destroy(model):
+        sql = connection.creation.sql_destroy_model(model, references, style)
+        cursor = connection.cursor()
+        for statement in sql:
+            cursor.execute(statement)
+
+    if func:
+        def inner(self, *args, **kwargs):
+            func(self, *args, **kwargs)
+            actual_destroy(self.model)
+        return inner
+    elif model:
+        actual_destroy(model)
+
+
+# TODO: pull into a common dev package so all armstrong code can use it
+def concrete(klass):
+    attrs = {'__module__': concrete.__module__, }
+    while True:
+        num = random.randint(1, 10000)
+        if num not in concrete.already_used:
+            break
+    return type("Concrete%s%d" % (klass.__name__, num), (klass, ), attrs)
+concrete.already_used = []
 
 
 class TestCase(DjangoTestCase):

a8363b74709853ac631afea1e2e14592ca7c9861
Travis Swicegood <development@domain51.com>
2011-05-10 11:16:31 -0500

    First pass at documenting this class

--- armstrong/core/arm_content/models.py
+++ armstrong/core/arm_content/models.py
@@ -7,6 +7,21 @@ from . import mixins
 
 
 class ContentBase(mixins.AuthorsMixin, mixins.PublicationMixin, models.Model):
+    """
+    The base class providing the basic "armstrong" behavior for a model.
+
+    This is provided as a way to handle cross-model querying.  For example, you
+    can use this to query across Article and Video models assuming they both
+    extend from a concrete implementation of this class.
+
+    This is *not* a concrete implementation.  This is to avoid having any
+    tables created that are not needed.  `armstrong.apps.content`_ provides a
+    concrete implementation of this if you want to use it without defining your
+    own base Content model.
+
+    .. _armstrong.apps.content: http://github.com/armstrongs/armstrong.apps.content
+    """
+
     title = models.CharField(max_length=255)
     summary = models.TextField()
 

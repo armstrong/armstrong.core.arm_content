6167e88dd0a02791aa20f5779c7ab0b175f82b52
Niran Babalola <niran@niran.org>
2011-05-16 19:46:09 -0500

    Add lettuce

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -11,11 +11,13 @@ settings = {
         'armstrong.core.arm_content',
         'armstrong.core.arm_content.tests.arm_content_support',
         'south',
+        'lettuce.django',
     ),
     'AUTH_PROFILE_MODULE': 'arm_content_support.SimpleProfile',
     'ROOT_URLCONF': 'armstrong.core.arm_content.tests.arm_content_support.urls',
     'ARMSTRONG_EXTERNAL_VIDEO_BACKEND': 'armstrong.core.arm_content.video.backends.YouTubeBackend'
 }
 
+full_name = "armstrong.core.arm_content"
 main_app = "arm_content"
 tested_apps = ("arm_content_support", "arm_content", )

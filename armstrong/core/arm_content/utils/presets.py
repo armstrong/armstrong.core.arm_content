54515d868d748686b0d1cddac40bd423a8d4b8b2
Niran Babalola <niran@niran.org>
2011-06-17 16:44:25 -0500

    Allow presets and defaults to be passed directly to functions that read those from the settings

--- armstrong/core/arm_content/utils/presets.py
+++ armstrong/core/arm_content/utils/presets.py
@@ -4,13 +4,13 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
 
-def get_preset_args(preset_label):
-    presets = getattr(settings, 'ARMSTRONG_PRESETS', {})
+def get_preset_args(preset_label, presets=None, defaults=None):
+    presets = presets or getattr(settings, 'ARMSTRONG_PRESETS', {})
 
     if not preset_label in presets:
         raise ImproperlyConfigured('The "%s" preset hasn\'t been '
             'configured in your settings module.' % preset_label)
     
-    args = deepcopy(getattr(settings, 'ARMSTRONG_PRESET_DEFAULTS', {}))
+    args = deepcopy(defaults or getattr(settings, 'ARMSTRONG_PRESET_DEFAULTS', {}))
     args.update(presets[preset_label])
     return args

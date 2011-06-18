476dc74e59c4c4f51b3df1a54dfd7bb39f49e044
Niran Babalola <niran@niran.org>
2011-06-18 03:22:15 -0500

    Add a stub render_visual to BaseImageMixin

--- armstrong/core/arm_content/mixins/images/base.py
+++ armstrong/core/arm_content/mixins/images/base.py
@@ -2,3 +2,6 @@ class BaseImageMixin(object):
     '''Properties of an image-centric content item.'''
     visual_type = 'image'
     is_visual_embedded = False
+
+    def render_visual(self, preset_label, presets=None, defaults=None, *args, **kwargs):
+        raise NotImplementedError()

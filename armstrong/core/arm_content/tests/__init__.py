81420149bba858397a1a6820b4cfb9096f184683
Travis Swicegood <development@domain51.com>
2011-06-22 17:03:24 -0500

    Remove a bunch of cruft from really old prototyping
    None of this is needed any longer.  Originally, we didn't have any
    concrete implementations of a content class.  For testing purposes we
    can assume that `armstrong.apps.content` is around (for now), so we're
    relying on it.
    
    The potential problem with this commit is for people who attempt to test
    `arm_content` via Django's test runner without having the main
    `armstrong.apps.content` app installed.  Opening a ticket to deal with
    that.

--- armstrong/core/arm_content/tests/__init__.py
+++ armstrong/core/arm_content/tests/__init__.py
@@ -1,31 +1,5 @@
-from ._utils import TestCase, create_random_article, create_random_video
-from .arm_content_support.models import BaseContent, Article, Video
 from .admin import *
 from .fields import *
 from .mixins import *
 from .models import *
 from .video import *
-
-
-class BaseContentTestCase(TestCase):
-    def test_can_add_content(self):
-        article = create_random_article()
-        Article.objects.get(id=article.id)
-
-    def test_can_fetch_combined_queryset(self):
-        create_random_article()
-        create_random_video()
-        create_random_article()
-        create_random_video()
-
-        self.assertEqual(BaseContent.objects.count(), 4)
-        for item in BaseContent.objects.all():
-            self.assertFalse(item.__class__ == BaseContent)
-
-        articles = [item for item in BaseContent.objects.all() if
-            item.__class__ == Article]
-        self.assertEqual(len(articles), 2)
-
-        videos = [item for item in BaseContent.objects.all() if
-            item.__class__ == Video]
-        self.assertEqual(len(videos), 2)

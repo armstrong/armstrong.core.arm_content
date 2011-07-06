7e701f8adb019aca9b075f3ecfb56cfaf6559876
Joshua Bonnett <jbonnett@baycitizen.org>
2011-07-06 11:26:30 -0700

    i am teh besst spelar
    double rr in parent for no good reason

--- armstrong/core/arm_content/fields/widgets/audio.py
+++ armstrong/core/arm_content/fields/widgets/audio.py
@@ -5,10 +5,10 @@ from django.forms.widgets import ClearableFileInput
 
 class AudioFileWidget(ClearableFileInput):
     def render(self, name, value, attrs):
-        parrent_output = super(AudioFileWidget, self).render(name, value, attrs)
+        parent_output = super(AudioFileWidget, self).render(name, value, attrs)
         from  ...fields.audio import AudioFile
         if type(value) is AudioFile:
             template_player = value.render()
         else:
             template_player = ''
-        return mark_safe(parrent_output + (template_player))
+        return mark_safe(parent_output + (template_player))

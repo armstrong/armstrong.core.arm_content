2cdfb8fd7c646fd524fb6ca4874775dd1f98106c
Joshua Bonnett <jbonnett@baycitizen.org>
2011-07-14 11:38:43 -0700

    added a Media class to the widget
    removed a cruftie __init__ that did nothing

--- armstrong/core/arm_content/fields/widgets/audio.py
+++ armstrong/core/arm_content/fields/widgets/audio.py
@@ -3,9 +3,14 @@ from django.forms.widgets import ClearableFileInput
 
 
 class AudioFileWidget(ClearableFileInput):
-    def __init__(self, *args, **kwargs):
-        super(AudioFileWidget, self).__init__(*args, **kwargs) 
-
+    class Media:
+        css = {
+                "all": (settings.STATIC_URL + "skin/jplayer.blue.monday.css",)
+        }
+        js = ("https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js",
+            settings.STATIC_URL + "js/jquery.jplayer.min.js"
+        }
+ 
     def render(self, name, value, attrs):
         self.attrs=attrs
         parent_output = super(AudioFileWidget, self).render(name, value, attrs)

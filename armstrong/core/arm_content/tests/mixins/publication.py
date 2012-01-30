dad163c5d23c1898bed4bd71a00e62a09ea4651d
Jon Cotton <jcotton@baycitizen.org>
2012-01-30 14:43:16 -0800

    Add tests for is_published

--- armstrong/core/arm_content/tests/mixins/publication.py
+++ armstrong/core/arm_content/tests/mixins/publication.py
@@ -7,17 +7,30 @@ from ..arm_content_support.models import ConcreteArticle
 import datetime
 
 class PublicationManagerTestCase(ArmContentTestCase):
+    def setUp(self):
+        self.published = ConcreteArticle.objects.create(
+                title="Published",
+                pub_date=datetime.datetime.now()-datetime.timedelta(days=1),
+                pub_status='P'
+            )
+        self.draft_art = ConcreteArticle.objects.create(
+                title="Not Published",
+                pub_date=datetime.datetime.now()-datetime.timedelta(days=1),
+                pub_status='D'
+            )
+        self.scheduled = ConcreteArticle.objects.create(
+                title="Future Published",
+                pub_date=datetime.datetime.now()+datetime.timedelta(days=1),
+                pub_status='P'
+            )
+
     def test_published_manager_only_pulls_published_content(self):
-        published = ConcreteArticle.objects.create(title="Published",
-                                           pub_date=datetime.datetime.now()-datetime.timedelta(days=1),
-                                           pub_status='P')
-        draft_art = ConcreteArticle.objects.create(title="Not Published",
-                                           pub_date=datetime.datetime.now()-datetime.timedelta(days=1),
-                                           pub_status='D')
-        scheduled = ConcreteArticle.objects.create(title="Future Published",
-                                           pub_date=datetime.datetime.now()+datetime.timedelta(days=1),
-                                           pub_status='P')
         all_published = ConcreteArticle.published.all().select_subclasses()
-        self.assertTrue(published in all_published)
-        self.assertTrue(not draft_art in all_published)
-        self.assertTrue(not scheduled in all_published)
+        self.assertTrue(self.published in all_published)
+        self.assertTrue(not self.draft_art in all_published)
+        self.assertTrue(not self.scheduled in all_published)
+
+    def test_is_published(self):
+        self.assertTrue(self.published.is_published)
+        self.assertFalse(self.draft_art.is_published)
+        self.assertFalse(self.scheduled.is_published)

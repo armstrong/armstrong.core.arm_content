d01453ad02b892090e043ef3055893d062922164
Dave McLain <dmclain@texastribune.org>
2012-02-29 19:31:33 -0600

    fix thumbnail filter

--- armstrong/core/arm_content/templatetags/content_helpers.py
+++ armstrong/core/arm_content/templatetags/content_helpers.py
@@ -1,9 +1,9 @@
 from django import template
-from armstrong.core.images.sorl import get_preset_thumbnail
+from armstrong.core.arm_content.images.sorl import get_preset_thumbnail
 
 register = template.Library()
 
 
 @register.filter
 def thumbnail(value, arg):
-    return get_preset_thumbnail(value, arg)
+    return get_preset_thumbnail(value, arg).url
